e4e77d4e2219f38858bac8fea99d80820fdceeaf
Alex Dima <alexdima@microsoft.com>
2018-03-02 13:01:53 +0100

    Fixes #1

--- extension.js
+++ extension.js
@@ -3,6 +3,15 @@ var copy = require('copy-paste').copy;
 
 function activate(context) {
 	var disposable = vscode.commands.registerCommand('copyRelativePath', function (uri) {
+		if (typeof uri === 'undefined') {
+			if (vscode.window.activeTextEditor) {
+				uri = vscode.window.activeTextEditor.document.uri;
+			}
+		}
+		if (!uri) {
+			vscode.window.showErrorMessage('Cannot copy relative path, as there appears no file is opened (or it is very large');
+			return;
+		}
 		var path = vscode.workspace.asRelativePath(uri);
 		path = path.replace(/\\/g, '/');
 		copy(path);
